b5fa926eef71df4c49f3cd919a2a1c7a9b536298
guosurui <13012615613@163.com>
2022-12-19 17:17:17 +0800

    添加背景

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,77 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from "vue-router";
 import HelloWorld from "./components/HelloWorld.vue";
+import { onMounted } from "vue";
+
+function background(bg: HTMLCanvasElement) {
+  let ctx = bg!.getContext("2d")!;
+  document.addEventListener("touchmove", function (e) {
+    e.preventDefault();
+  });
+
+  let pixelRatio = window.devicePixelRatio || 1,
+    width = window.innerWidth,
+    height = window.innerHeight,
+    f = 90,
+    q: { x: any; y: number }[],
+    r = 0,
+    u = Math.PI * 2,
+    v = Math.cos,
+    z = Math.random;
+
+  bg.width = width * pixelRatio;
+  bg.height = height * pixelRatio;
+  ctx.scale(pixelRatio, pixelRatio);
+  ctx.globalAlpha = 0.6;
+
+  function i() {
+    ctx.clearRect(0, 0, width, height);
+    q = [
+      { x: 0, y: height * 0.7 + f },
+      { x: 0, y: height * 0.7 - f },
+    ];
+    while (q[1].x < width + f) d(q[0], q[1]);
+  }
+
+  function d(i: { x: any; y: any }, j: { x: any; y: any }) {
+    ctx.beginPath();
+    ctx.moveTo(i.x, i.y);
+    ctx.lineTo(j.x, j.y);
+    let k = j.x + (z() * 2 - 0.25) * f,
+      n = y(j.y);
+    ctx.lineTo(k, n);
+    ctx.closePath();
+    r -= u / -50;
+    ctx.fillStyle =
+      "#" +
+      (
+        ((v(r) * 127 + 128) << 16) |
+        ((v(r + u / 3) * 127 + 128) << 8) |
+        ((v(r + (u / 3) * 2) * 127 + 128) << 0)
+      ).toString(16);
+    ctx.fill();
+    q[0] = q[1];
+    q[1] = { x: k, y: n };
+  }
+
+  function y(p: number): number {
+    let t = p + (z() * 2 - 1.1) * f;
+    return t > height || t < 0 ? y(p) : t;
+  }
+
+  document.onclick = i;
+  document.ontouchstart = i;
+  i();
+}
+
+onMounted(() => {
+  background(document.getElementsByTagName("canvas")[0] as HTMLCanvasElement);
+});
 </script>
 
 <template>
   <header>
+    <canvas class="bg" ref="bg"></canvas>
     <img
       alt="Vue logo"
       class="logo"
@@ -27,6 +94,15 @@ import HelloWorld from "./components/HelloWorld.vue";
 </template>
 
 <style scoped>
+.bg {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  z-index: -1;
+}
+
 header {
   line-height: 1.5;
   max-height: 100vh;
